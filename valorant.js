d17ef5f94a42b38702670a1b9ad2bb09eb1210a5
Fernanda Machado <fernandasilvamachado100@gmail.com>
2024-09-25 23:39:39 -0300

    Create valorant.js

---
+++ valorant.js
@@ -0,0 +1,8 @@
+let publico= parseInt(prompt('Quantas pessoas irão ao evento?'))
+let valorfinal
+
+for(var ingresso=150;ingresso>0;ingresso-=10)
+var bruto-ingresso publico-1000
+valor=ingresso
+}
+alert('O valor do ingresso será ${valor} reais e o lucro líquido será ${bruto}`)
