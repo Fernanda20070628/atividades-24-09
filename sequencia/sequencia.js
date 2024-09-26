682c7a0c061ac7edd453ba81148309a392c38ad7
Fernanda Machado <fernandasilvamachado100@gmail.com>
2024-09-25 21:12:40 -0300

    Update sequencia.js

--- sequencia/sequencia.js
+++ sequencia/sequencia.js
@@ -1,8 +1,9 @@
 let numero= 1 ; 
-
+var somatotal = 0 
 for ( let s=1; s <=20; s++){
 let soma = 1+2*numero
 numero=soma
 console.log (soma)
-
+somatotal =somatotal+soma 
 }
+console.log(`A soma total da sequencia é ${somatotal}`) 
